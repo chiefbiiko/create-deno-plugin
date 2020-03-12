5f021afb005925ec03454aa1d02feb36922f46bc
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-03-12 20:17:48 +0100

    deps: update to std@v0.36.0

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { encode, decode } from "https://deno.land/std@v0.35.0/strings/mod.ts";
-export { existsSync } from "https://deno.land/std@v0.35.0/fs/mod.ts";
-export { parse } from "https://deno.land/std@v0.35.0/flags/mod.ts";
-export { basename, join } from "https://deno.land/std@v0.35.0/path/mod.ts";
+export { encode, decode } from "https://deno.land/std@v0.36.0/strings/mod.ts";
+export { existsSync } from "https://deno.land/std@v0.36.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@v0.36.0/flags/mod.ts";
+export { basename, join } from "https://deno.land/std@v0.36.0/path/mod.ts";
